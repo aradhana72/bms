41b6556479a7041f0cdfc0693129c23833f3d23f
aradhana72 <aradhanakund@gmail.com>
2021-12-09 18:06:53 +0530

    HOC AND LAYOUTS

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
-import {Route, Routes, BrowserRouter} from "react-router-dom";
+import { Route } from "react-router-dom";
+
 //HOC
 import DefaultHOC from "./HOC/Default.HOC";
 
-//Components
+//Component
 import Temp from "./components/temp";
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-    <Routes>
-      <DefaultHOC path="/" exact element={<Temp/>}/>
-      </Routes>
-      </BrowserRouter>
+     <DefaultHOC path="/" exact component={Temp} />
     </>
   );
 }
-
+// / , /movie
 export default App;
