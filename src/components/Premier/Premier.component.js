25b978bfa2eb338ff4ea81d9a4ed65d6e9b5d103
aradhana72 <aradhanakund@gmail.com>
2021-12-14 18:38:37 +0530

    Premier

--- src/components/Premier/Premier.component.js
+++ src/components/Premier/Premier.component.js
@@ -18,71 +18,75 @@ const Premier = () => {
       alt: "Premier Image",
       title: "Dune",
       subtitle: "English",
-      isDark: false
+
     },
     {
       src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00122526-laqcblhanx-portrait.jpg",
       alt: "Premier Image",
       title: "Dune",
       subtitle: "English",
-      isDark: false
+
     },
     {
       src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00122526-laqcblhanx-portrait.jpg",
       alt: "Premier Image",
       title: "Dune",
       subtitle: "English",
-      isDark: false
+
     },
     {
       src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00122526-laqcblhanx-portrait.jpg",
       alt: "Premier Image",
       title: "Dune",
       subtitle: "English",
-      isDark: false
+
     },
     {
       src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00122526-laqcblhanx-portrait.jpg",
       alt: "Premier Image",
       title: "Dune",
       subtitle: "English",
-      isDark: false
+
     },
     {
       src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00122526-laqcblhanx-portrait.jpg",
       alt: "Premier Image",
       title: "Dune",
       subtitle: "English",
-      isDark: false
+
     },
     {
       src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00122526-laqcblhanx-portrait.jpg",
       alt: "Premier Image",
       title: "Dune",
       subtitle: "English",
-      isDark: false
+
     },
     {
       src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00122526-laqcblhanx-portrait.jpg",
       alt: "Premier Image",
       title: "Dune",
       subtitle: "English",
-      isDark: false
+
     },
     {
       src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00122526-laqcblhanx-portrait.jpg",
       alt: "Premier Image",
       title: "Dune",
       subtitle: "English",
-      isDark: false
+
     }
   ];
 
   return (
       <>
+      <div className="flex flex-col items-start gap-1 py-4">
+        <h3 className="text-white text-xl font-bold">Premieres</h3>
+        <p className="text-white text-sm">Brand New releases every friday</p>
+      </div>
         <Slider {...settings} >
         {PremierImages.map((image)=> (
-          <Poster {...image} />
+          <Poster {...image} isDark/>
         ))}
         </Slider>
       </>
