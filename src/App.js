e12e0239091dac1e92a23937f488c839a41b6634
aradhana72 <aradhanakund@gmail.com>
2021-12-14 19:10:37 +0530

    Movie Page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import { Route } from "react-router-dom";
 
 //HOC
 import DefaultHOC from "./HOC/Default.HOC";
+import MovieHOC from "./HOC/Movie.HOC";
 
-//Component
+//Pages
 import HomePage from "./pages/Home.page";
+import Movie from "./pages/Movie.page";
 
 function App() {
   return (
     <>
      <DefaultHOC path="/" exact component={HomePage} />
+     <MovieHOC path="/movie/:id" exact component={Movie} />
     </>
   );
 }
